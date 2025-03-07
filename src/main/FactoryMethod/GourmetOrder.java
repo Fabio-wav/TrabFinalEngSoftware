0868ba183fb2f19f100839fb32ec73c0c540bf8e
¨fabio-wav¨ <fabio.vale@estudante.ufjf.br>
2025-03-07 12:53:33 -0300

    Changes2

---
+++ src/main/FactoryMethod/GourmetOrder.java
@@ -0,0 +1,26 @@
+package main.FactoryMethod;
+import main.Singleton.*;
+
+
+public class GourmetOrder extends Order {
+
+    public GourmetOrder() {
+        this.description = "Pedido Gourmet";
+    }
+
+    @Override
+    public void prepare() {
+        Logger.getInstance().log("Preparando pedido gourmet.");
+        setState(new PreparingState(this));
+    }
+
+    @Override
+    public OrderItem createItem(String type) {
+        if(type.equalsIgnoreCase("bife")) {
+            return new Steak();
+        } else if(type.equalsIgnoreCase("salada")) {
+            return new Salad();
+        }
+        return null;
+    }
+}
